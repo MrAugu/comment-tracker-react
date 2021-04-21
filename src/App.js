a1ce068a32b19de0473f8624c21983d4a93ac9e5
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-04-21 22:07:10 +0300

    Adding Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import './App.css';
+import NavBar from "./components/NavBar";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import User from "./views/User";
+import UserEdit from "./views/UserEdit";
+import {
+  BrowserRouter as Router,
+  Switch, Route
+} from "react-router-dom";
 
 class App extends React.Component {
   render () {
     return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <Router>
+        <Switch>
+          <NavBar />
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/user" component={User} />
+          <Route path="/user/edit" component={UserEdit} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,8 +3,14 @@ import React from "react";
 class Home extends React.Component {
   render () {
     return (
-      <div>
-        <p>Home page.</p>
+      <div className="section">
+        <div className="container">
+          <h1 className="title">Comment Tracker</h1>
+          <p className="subtitle block">A react app with a node.js backend written using the fastify framework created by MrAugu#7917 [mraugu@yahoo.com] in order to learn and work hands-on with these technologies.</p>
+          <a className="button is-link block" href="https://github.com/MrAugu/comment-tracker-react">React App Source on GitHub</a>
+          <a className="button is-info block" href="https://github.com/MrAugu/comment-tracker" style={{ marginLeft: "1em" }}>Backend Source on GitHub</a>
+          <br></br><a className="button is-ghost block" href="/">Go to signup page</a>
+        </div>
       </div>
     );
   }
