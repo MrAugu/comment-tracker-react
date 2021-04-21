208a6b40149a2ff6830527ad53a63f93d2849cc9
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-04-21 20:58:37 +0300

    Cleanup

---
+++ src/views/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class User extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>User page.</p>
+      </div>
+    );
+  }
+}
+
+export default User;
---
+++ src/views/UserEdit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserEdit extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>User edit page.</p>
+      </div>
+    );
+  }
+}
+
+export default UserEdit;
